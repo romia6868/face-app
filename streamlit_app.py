19a1c5bbcd0ad3d51c3d0c1b49b58ab68a16872e
romia6868 <romia6868@gmail.com>
2026-03-09 20:22:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,48 @@
 import streamlit as st
 import tensorflow as tf
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras import layers, models
 
 class L2Normalize(tf.keras.layers.Layer):
     def call(self, inputs):
         return tf.math.l2_normalize(inputs, axis=1)
 
+def build_pro_embedding():
+
+    base_model = MobileNetV2(
+        input_shape=(128,128,3),
+        include_top=False,
+        weights="imagenet"
+    )
+
+    base_model.trainable = False
+
+    model = models.Sequential([
+        base_model,
+        layers.GlobalAveragePooling2D(),
+
+        layers.Dense(512, activation="relu"),
+        layers.BatchNormalization(),
+        layers.Dropout(0.3),
+
+        layers.Dense(256, activation="relu"),
+        layers.BatchNormalization(),
+
+        layers.Dense(128),
+        L2Normalize()
+    ])
+
+    return model
+
+
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model(
-        "face_encoder.keras",
-        custom_objects={"L2Normalize": L2Normalize},
-        compile=False
-    )
+    model = build_pro_embedding()
+    model.load_weights("face_encoder.weights.h5")
     return model
+
 
 model = load_model()
 
-st.title("Face Recognition App")
-st.success("Model loaded successfully")
-
-
+st.title("Face Recognition")
+st.success("Model loaded")
