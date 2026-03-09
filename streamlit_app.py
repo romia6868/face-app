8d0c3e03095d6aa6f87c097b3e84ebbd8a767fbc
romia6868 <romia6868@gmail.com>
2026-03-09 19:29:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,28 +2,27 @@ import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-# אם יש Lambda layer צריך להגדיר את הפונקציה שלה
+# פונקציה של ה-Lambda layer
 def euclidean_distance(vectors):
     x, y = vectors
     return tf.sqrt(tf.reduce_sum(tf.square(x - y), axis=1, keepdims=True))
 
-# טעינת המודל עם cache
+# טעינת המודל
 @st.cache_resource
 def load_face_model():
 
     model = load_model(
-        "/content/drive/MyDrive/Presence_Project/my_siamese2_model.h5",                 # שם קובץ המודל שלך
-        compile=False,              # מונע בעיות תאימות
+        "my_siamese2_model.h5",
+        compile=False,
         custom_objects={
             "euclidean_distance": euclidean_distance
-        }                           # מאפשר Lambda layer
+        }
     )
 
     return model
 
-
-# טעינת המודל
 model = load_face_model()
+
 
 st.title("Face Recognition App")
 
