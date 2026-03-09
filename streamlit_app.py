015f3e5a48aae863df5f0e4a0b719364df804195
romia6868 <romia6868@gmail.com>
2026-03-09 19:46:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,17 @@
 import streamlit as st
 import tensorflow as tf
-from tensorflow.keras.models import load_model
 
-# פונקציית המרחק של ה-Siamese
 def euclidean_distance(vectors):
     x, y = vectors
     return tf.sqrt(tf.reduce_sum(tf.square(x - y), axis=1, keepdims=True))
 
 @st.cache_resource
 def load_face_model():
-
     model = tf.keras.models.load_model(
-        "my_siamese2_model.h5",
-        compile=False,
-        custom_objects={
-            "euclidean_distance": euclidean_distance
-        },
-        safe_mode=False   # חשוב ב-Keras 3 כשיש Lambda layer
+        "my_siamese2_model.keras",
+        custom_objects={"euclidean_distance": euclidean_distance},
+        compile=False
     )
-
     return model
 
 model = load_face_model()
